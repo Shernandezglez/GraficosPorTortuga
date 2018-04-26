8ccccc8b1f308e22b070824ff1c1255a3c46d5fe
Shernandezglez <32920669+Shernandezglez@users.noreply.github.com>
2018-04-25 21:47:53 -0500

    Add files via upload

---
+++ GraficosPorTortuga/GraficosPorTortuga/Tortuga.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficosPorTortuga
+{
+    class Tortuga
+    {
+        private bool[,] _matr;
+        private bool pluma;
+
+        public Tortuga(int m, int n)
+        {
+            _matr = new bool[m, n];
+            pluma = true;
+        }
+
+        public void inicializarMatriz()
+        {
+            for(int r = 0; r < _matr.Length -1; r++)
+            {
+                for(int c = 0; c < _matr.Length-1; c++)
+                {
+                    _matr[r, c] = false;
+                }
+            }
+        }
+
+        public bool plumaArriba()
+        {
+            if(pluma == false)
+            {
+                pluma = true;
+            }
+            return pluma;
+
+        }
+
+        public bool plumaAbajo()
+        {
+            if (pluma == true)
+            {
+                pluma = false;
+            }
+            return pluma;
+
+        }
+
+        public void llenarMatriz()
+        {
+            if(pluma == false)
+            {
+                for (int r = 0; r < _matr.Length - 1; r++)
+                {
+                    for (int c = 0; c < _matr.Length - 1; c++)
+                    {
+                        _matr[r, c] = true;
+                    }
+                }
+            }
+
+            else
+            {
+                for (int r = 0; r < _matr.Length - 1; r++)
+                {
+                    for (int c = 0; c < _matr.Length - 1; c++)
+                    {
+                        _matr[r, c] = false;
+                    }
+                }
+            }
+        }
+
+
+        public string mostrarMatriz()
+        {
+            string cosa = "";
+
+            for (int r = 0; r < _matr.Length - 1; r++)
+            {
+                for (int c = 0; c < _matr.Length - 1; c++)
+                {
+                   
+                }
+            }
+            return cosa;
+        }
+    }
+}
